a8e5a517d45970b35218ba1760bb407127e497c3
Alvin Chow <alvinchow86@gmail.com>
2022-02-25 03:15:20 -0800

    Redo migrations

--- monarch/admin.py
+++ monarch/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 # from django.contrib.auth.admin import UserAdmin
-from monarch.db.models import User
+from monarch.db.models import User, Account, Category, Transaction
 
 admin.site.register(User)
+admin.site.register(Account)
+admin.site.register(Category)
+admin.site.register(Transaction)
