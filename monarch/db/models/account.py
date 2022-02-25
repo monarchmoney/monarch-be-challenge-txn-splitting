7beda85e4d3116db321a9f4505c1f6a98506ddeb
Alvin Chow <alvinchow86@gmail.com>
2022-02-24 20:46:20 -0800

    Account, Transaction models

---
+++ monarch/db/models/account.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Account(models.Model):
+    class Meta:
+        db_table = "account"
+
+    data_provider_id = models.TextField(blank=True, null=True, unique=True)
+
+    # Primary data
+    name = models.TextField(blank=True, null=True)  # only non-blank when user edited name
+    type = models.TextField()
+
+    # current_balance = models.DecimalField(max_digits=16, decimal_places=2, blank=True, null=True)
+
+    user = models.ForeignKey(
+        "monarch.User",
+        related_name="accounts",
+        on_delete=models.CASCADE,
+    )
---
+++ monarch/db/models/transaction.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Transaction(models.Model):
+    class Meta:
+        db_table = "account"
+
+    data_provider_id = models.TextField(blank=True, null=True, unique=True)
+
+    # Primary data
+    description = models.TextField(blank=True, null=True)  # only non-blank when user edited name
+    amount = models.DecimalField(max_digits=16, decimal_places=2)
+    date = models.DateField()
+    # category = models.ForeignKey(
+    #     "monarch.Category",
+    #    related_name="transactions",
+        # on_delete=models.PROTECT,
+    # )
+
+    user = models.ForeignKey(
+        "monarch.User",
+        related_name="accounts",
+        on_delete=models.CASCADE,
+    )
